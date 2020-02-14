45df6e4665f4530259bf03b210f3542828e4a50d
CaoKang <caokang@outlook.com>
2020-02-14 16:56:45 +0800

    完善代码

--- src/Order/ResponseAttribute/SignalAgentPayResponse.php
+++ src/Order/ResponseAttribute/SignalAgentPayResponse.php
@@ -16,5 +16,80 @@ use Zeevin\Libaip\Core\BaseResponseAttribute;
 
 class SignalAgentPayResponse extends BaseResponseAttribute
 {
+    /**
+     *
+     * @JMS\XmlElement(cdata=false)
+     * @SerializedName("order_no")
+     * @JMS\Type("string")
+     */
+    protected $order_no;
+    /**
+     *
+     * @JMS\XmlElement(cdata=false)
+     * @SerializedName("pay_whereabouts")
+     * @JMS\Type("string")
+     */
+    protected $pay_whereabouts;
+    /**
+     *
+     * @JMS\XmlElement(cdata=false)
+     * @SerializedName("extend_info")
+     * @JMS\Type("string")
+     */
+    protected $extend_info;
+    /**
+     *
+     * @JMS\XmlElement(cdata=false)
+     * @SerializedName("pay_status")
+     * @JMS\Type("string")
+     */
+    protected $pay_status;
+    /**
+     *
+     * @JMS\XmlElement(cdata=false)
+     * @SerializedName("biz_order_no")
+     * @JMS\Type("string")
+     */
+    protected $biz_order_no;
+
+    /**
+     * @return mixed
+     */
+    public function getOrderNo()
+    {
+        return $this->order_no;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPayWhereabouts()
+    {
+        return $this->pay_whereabouts;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getExtendInfo()
+    {
+        return $this->extend_info;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPayStatus()
+    {
+        return $this->pay_status;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBizOrderNo()
+    {
+        return $this->biz_order_no;
+    }
 
 }
